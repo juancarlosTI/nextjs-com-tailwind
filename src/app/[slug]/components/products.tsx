c5b1ba70fd168488fea280253d567ab34cc6ff04
Juan Carlos <juancarlosti@rede.ulbra.br>
2025-02-24 13:41:51 -0300

    feat: listing products fixed bug - import error

--- src/app/[slug]/components/products.tsx
+++ src/app/[slug]/components/products.tsx
@@ -1,43 +1,47 @@
 import { Product } from "@prisma/client";
-import { Link } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductsProps {
     products: Product[]
 }
 
 const Products = ({ products }: ProductsProps) => {
-    return <div className="space-y-3 px-5 py-3">
-        {products.map(product => (
-            <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3 border-b">
-                
-                {/* Esqureda */}
-                <div>
-                    <h3 className="text-sm font-medium">
-                        {product.name}
-                    </h3>
-                    <p className="line-clamp-2 text-sm text-muted-foreground">
-                        {product.description}
-                    </p>
-                    <p className="pt-3 text-sm font-semibold">
-                        {new Intl.NumberFormat("pt-BR", {
-                        style:'currency',
-                        currency:'BRL'
-                    }).format(product.price)}</p>
-                </div>
+    return (
+        <div className="space-y-3 px-5 py-3">
+            {products.map(product => (
+                <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3">
+                    {/* Esquerda */}
+                    <div>
+                        <h3 className="text-sm font-medium">
+                            {product.name}
+                        </h3>
+                        <p className="line-clamp-2 text-sm text-muted-foreground">
+                            {product.description}
+                        </p>
+                        <p className="pt-3 text-sm font-semibold">
+                            {new Intl.NumberFormat("pt-BR", {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(product.price)}</p>
+                    </div>
 
-                {/* Direita */}
-                <div className="relative min-h-[82px] min-w-[120px]">
-                    <Image
-                        src={product.imageUrl}
-                        alt={product.name}
-                        fill
-                        className="rounded-lg object-contain"
-                    />
-                </div>
-            </Link>
-        ))}
-    </div>
+                    {/* Direita */}
+                    <div className="relative min-h-[82px] min-w-[120px]">
+                        <Image
+                            src={product.imageUrl}
+                            alt={product.name}
+                            fill
+                            className="rounded-lg object-contain"
+                        />
+                    </div>
+                </Link>
+            ))
+            }
+
+
+        </div >
+    )
 }
 
 export default Products;
